612b1ee65a5675f5c2f5dab757cf4fd99ad146ac
Nils Danielsen <nils.danielsen@hiq.se>
2025-03-24 10:54:55 +0100

    added generated spring-app

---
+++ src/main/kotlin/calculator/mathy/MathyApplication.kt
@@ -0,0 +1,11 @@
+package calculator.mathy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MathyApplication
+
+fun main(args: Array<String>) {
+	runApplication<MathyApplication>(*args)
+}
---
+++ src/test/kotlin/calculator/mathy/MathyApplicationTests.kt
@@ -0,0 +1,13 @@
+package calculator.mathy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MathyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
